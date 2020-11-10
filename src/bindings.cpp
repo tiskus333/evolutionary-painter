4f65ad791a543df26a73d0dbcec43feacb2cbee0
pktiuk <kotiuk@zohomail.eu>
2020-11-10 16:18:00 +0100

    Add bindings for conversion of C++ vectors

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -1,10 +1,25 @@
 #include <boost/python.hpp>
 #include <boost/python/raw_function.hpp>
+#include <vector>
 
 #include "Member.hpp"
 #include <EvolAlg.hpp>
 
 char const *greet() { return "hello World"; }
+
+template <class T> struct VecToList
+{
+    static PyObject *convert(const std::vector<T> &vec)
+    {
+        boost::python::list *l = new boost::python::list();
+        for (size_t i = 0; i < vec.size(); i++)
+        {
+            l->append(vec[i]);
+        }
+
+        return l->ptr();
+    }
+};
 
 namespace python = boost::python;
 
@@ -21,4 +36,6 @@ BOOST_PYTHON_MODULE(bindings)
         .def("stop", &EvolAlg::stop)
         .def("run", &EvolAlg::run);
     python::def("greet", greet);
+    python::to_python_converter<std::vector<std::string, std::allocator<std::string>>, VecToList<std::string>>();
+    python::to_python_converter<std::vector<int, std::allocator<int>>, VecToList<int>>();
 }
