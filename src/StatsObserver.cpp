8dd8c773d19920579354638896a89a97f834a7a0
pktiuk <kotiuk@zohomail.eu>
2020-11-16 20:29:47 +0100

    Fix mixed values

--- src/StatsObserver.cpp
+++ src/StatsObserver.cpp
@@ -7,8 +7,8 @@ StatsObserver::~StatsObserver() {}
 void StatsObserver::update()
 {
     lock guard(lists_mutex_);
-    fitness_list_.push_back(observed_EvolAlg_->getGeneration());
-    generation_list_.push_back(observed_EvolAlg_->getbestFitness());
+    generation_list_.push_back(observed_EvolAlg_->getGeneration());
+    fitness_list_.push_back(observed_EvolAlg_->getbestFitness());
 }
 
 void StatsObserver::setObservedEvolAlg(EvolAlg &p)
